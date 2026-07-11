79cba7e3e700f0acfe17b9956b9d9372a8c29ddc
Okpabi Counsel <okpabicounsel@gmail.com>
2026-07-11 13:08:16 +0100

    fix: fix merge conflict and build issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,18 +5,7 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-<<<<<<< HEAD
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-=======
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
->>>>>>> provider/main
 ]);
 
 export default eslintConfig;
